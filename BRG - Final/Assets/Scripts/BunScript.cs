c7034f07328f77ed78f242964c3c542c8da748cf
NRS229 <noah229@hotmail.com>
2020-02-07 15:34:54 -0600

    "Playable" project
    First real commit of the project

---
+++ BRG - Final/Assets/Scripts/BunScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunScript : MonoBehaviour
+{
+    public float jumpForce;
+    private bool jump;
+    private bool bunsTouching;
+    private bool upperBunTouchingTopping;
+    Rigidbody2D rbBunUp;
+    public Rigidbody2D rbBunDown;
+
+
+    void Start()
+    {
+        rbBunUp = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is for input
+    void Update()
+    {
+        if ((Input.GetButtonDown("Jump") || Input.touchCount > 0) & (bunsTouching || upperBunTouchingTopping))
+            jump = true;
+    }
+  
+    //FixedUpdate is for applying physics
+    void FixedUpdate()
+    {
+        if (jump)
+        {
+            jump = false;
+            // Add a vertical force (Jump)
+            rbBunUp.AddForce(new Vector2(0.0f, jumpForce * 35f));
+            rbBunDown.AddForce(new Vector2(0.0f, jumpForce * 25f));
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "LowerBun")
+        {
+            bunsTouching = true;
+        }
+        if (collision.gameObject.name == "Topping")
+        {
+            upperBunTouchingTopping = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "LowerBun")
+        {
+            bunsTouching = false;
+        }
+        if (collision.gameObject.name == "Topping")
+        {
+            upperBunTouchingTopping = false;
+        }
+
+    }
+}
---
+++ BRG - Final/Assets/Scripts/ToppingScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToppingScript : MonoBehaviour
+{
+    public float speed;
+    private bool partOfBurger;
+    public GameObject NewToppingPrefab;
+    private GameObject NextTopping;
+    public Sprite[] ToppingSprites;
+
+    void FixedUpdate()
+    {
+        if(!partOfBurger)
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        else
+        {
+            gameObject.name = "Topping";
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "UpperBun" & !partOfBurger)
+        {
+            BecomePartOfBurger();
+            InstantiateTopping();
+            //float newToppingY = gameObject.transform.position.y + 0.35f;
+            //ManagerSC.instance.InstantiateTopping(newToppingY);
+            //ManagerSC.instance.RandomNextToppingSprite();
+        }
+    }
+
+    private void BecomePartOfBurger() 
+    {
+        partOfBurger = true;
+        gameObject.AddComponent<Rigidbody2D>();
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+        gameObject.transform.SetParent(GameObject.Find("UpperBun").transform);
+        //ManagerSC.instance.IncreaseScore();
+    }
+
+    private void InstantiateTopping()
+    {
+        //float x = GameObject.Find("UpperBun").transform.position.x + Random.Range(10, 16);
+        //float y = newToppingY;
+        float x = transform.position.x + Random.Range(10, 16);
+        float y = transform.position.y + 0.46f;
+
+        NextTopping = Instantiate(NewToppingPrefab, new Vector2(x, y), Quaternion.identity) as GameObject;
+        NextTopping.GetComponent<ToppingScript>().NewToppingPrefab = NewToppingPrefab;
+    }
+
+}
