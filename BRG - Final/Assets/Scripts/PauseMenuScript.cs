982b36e028d7603220234c64901732f2b76608e6
NRS229 <noah229@hotmail.com>
2020-02-19 13:05:43 -0600

    Added play/pause button

--- BRG - Final/Assets/Scripts/PauseMenuScript.cs
+++ BRG - Final/Assets/Scripts/PauseMenuScript.cs
@@ -1,37 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
     public static bool isPaused = false;
     public GameObject pauseMenuUI;
-
+    public Button pauseResumeButton;
+    public Sprite pauseSprite;
+    public Sprite playSprite;
+    
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape)){
-            if(isPaused){
-                Resume();
-            }
-            else{
-                Pause();
-            }
+            ResumeOrPause();
         }
         
     }
 
+    public void ResumeOrPause(){
+        if(isPaused){
+            Resume();
+        }
+        else{
+            Pause();
+        }
+    }
     public void Resume(){
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        pauseResumeButton.GetComponent<Image>().sprite = pauseSprite;
     }
 
     void Pause(){
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+        pauseResumeButton.GetComponent<Image>().sprite = playSprite;
     }
 
     public void LoadMenu(){
